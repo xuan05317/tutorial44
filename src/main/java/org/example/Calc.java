53bb34b618f9724401d3ff4f4c9adffb1fd96b56
xuan05317 <3208393479@qq.com>
2025-10-19 17:41:13 +0800

    Fix subtract method: change x*y to x-y (tests pass now)

--- src/main/java/org/example/Calc.java
+++ src/main/java/org/example/Calc.java
@@ -1,14 +1,12 @@
 package org.example;
 
 public class Calc {
-
     public int add(int x, int y)
     {
-        return x+y;
+        return x + y;
     }
-
     public int subtract(int x, int y)
     {
-        return x*y;
+        return x - y; // 正确逻辑：返回“被减数x - 减数y”的结果
     }
 }
